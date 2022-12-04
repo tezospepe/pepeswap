c3fc5e94a2b676200550ff448af44046780ab89c
Andrew Rubenstein <andjruben@gmail.com>
2022-12-04 16:41:13 -0500

    create Swap boilerplate

--- src/app/pages/Swap/index.tsx
+++ src/app/pages/Swap/index.tsx
@@ -1,5 +1,21 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { PageWrapper } from 'app/components/PageWrapper';
+import { SwapWidget } from './components/SwapWidget';
 
 export function Swap() {
-  return <></>;
+  return (
+    <>
+      <Helmet>
+        <title>Swap</title>
+        <meta
+          name="description"
+          content="SpicySwap is a next-generation DEX built by Genius Contracts specifically for token-to-token swaps on Tezos. SpicySwap is governed by SalsaDAO ($sDAO)."
+        />
+      </Helmet>
+      <PageWrapper>
+        <SwapWidget />
+      </PageWrapper>
+    </>
+  );
 }
