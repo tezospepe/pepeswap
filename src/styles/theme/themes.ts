c4df2611e192adeea00bbb12416f43acc97941f5
Andrew Rubenstein <andjruben@gmail.com>
2023-03-11 23:15:44 -0500

    fix: theme colors broken when called from hook

--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -12,8 +12,8 @@ const darkTheme: Theme = {
   primary: 'rgba(30,31,40,1)',
   text: 'rgba(149,215,146,1)',
   textSecondary: 'rgba(252,253,252,1)',
-  background: 'rgba(5,5,15,1);',
-  backgroundVariant: 'rgba(149,215,146,1);',
+  background: 'rgba(5,5,15,1)',
+  backgroundVariant: 'rgba(149,215,146,1)',
   border: 'rgba(241,233,231,0.15)',
   borderLight: 'rgba(241,233,231,0.05)',
 };
