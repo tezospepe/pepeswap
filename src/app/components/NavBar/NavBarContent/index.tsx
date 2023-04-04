c8bcdbf69796d78249a248705bd771da91cb038b
Andrew Rubenstein <andjruben@gmail.com>
2023-04-03 21:32:16 -0400

    fix: open mobile nav toggle if not connected

--- src/app/components/NavBar/NavBarContent/index.tsx
+++ src/app/components/NavBar/NavBarContent/index.tsx
@@ -33,7 +33,7 @@ export function NavBarContent({
   isProfileActive,
 }: NavBarContentProps) {
   const handleHamburgerClick = () => {
-    if (connected) {
+    if (!connected) {
       toggleModal();
     }
   };
